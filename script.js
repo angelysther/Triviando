41f1a098de8549b9ffa73742e92235291a51a30b
Angelys Rodriguez <angelysther@gmail.com>
2020-09-24 02:53:50 +0000

    Modificación en pantallas de educacion y logica, incorporando js funcional

--- script.js
+++ script.js
@@ -1,117 +1,117 @@
-function resultado_educacion() {
-    var p1, p2, p3, p4, p5, nota;
+var resultados = 0;
 
-    <!-- 1ª pregunta -->
-    if (document.getElementById("p12").checked == true) {
-        p1 = 1;
-    }
-    if (p1 == 1) {
-        document.querySelectorAll(".pregunta1")[1].style.color = "#00FF00";
-    } else {
-        p1 = 0;
-    }
-    <!-- 2ª pregunta -->
-    if (document.getElementById("p21").checked == true) {
-        p2 = 1;
-    }
-    if (p2 == 1) {
-        document.querySelectorAll(".pregunta2")[0].style.color = "#00FF00";
-    } else {
-        p2 = 0;
-    }
-    <!-- 3ª pregunta -->
-    if (document.getElementById("p33").checked == true) {
-        p3 = 1;
-    }
-    if (p3 == 1) {
-        document.querySelectorAll(".pregunta3")[2].style.color = "#00FF00";
-    } else {
-        p3 = 0;
-    }
-    <!-- 4ª pregunta -->
-    if (document.getElementById("p42").checked == true) {
-        p4 = 1;
-    }
-    if (p4 == 1) {
-        document.querySelectorAll(".pregunta4")[1].style.color = "#00FF00";
-    } else {
-        p4 = 0;
-    }
-    <!-- 5ª pregunta -->
-    if (document.getElementById("p51").checked == true) {
-        p5 = 1;
-    }
-    if (p5 == 1) {
-        document.querySelectorAll(".pregunta5")[0].style.color = "#00FF00";
-    } else {
-        p5 = 0;
-    }
-
-    nota = p1 + p2 + p3 + p4 + p5;
-    document.getElementById("resultado").innerHTML = "Aciertos: " + nota;
-    //  rs = confirm("C o n t i n u a r . . .") CREAR UN ALERT;
-    rs = true;
-    if (rs == false) {
-        document.forms[0].reset();
-        location.reload();
-    }
+function onloadFunction() {
+    document.getElementById("preguntados").style.display = "none";
+    document.getElementById("preguntatres").style.display = "none";
+    document.getElementById("preguntacuatro").style.display = "none";
+    document.getElementById("preguntacinco").style.display = "none";
+    document.getElementById("boton_resultado").style.display = "none";
+    document.getElementById("resultado").style.display = "none";
+    document.getElementById("boton_retornar").style.display = "none";
 }
 
-function resultado_logica() {
-    var p1, p2, p3, p4, p5, nota;
+function myFunctionAvanzarSegunda() {
+    if ((null != document.getElementById("r_l_12") && document.getElementById("r_l_12").checked) ||
+        (null != document.getElementById("r_e_12") && document.getElementById("r_e_12").checked)) {
+        resultados = resultados + 1;
+        console.log("resultado_1  " + resultados);
+    }
+    document.getElementById("preguntauno").style.display = "none";
+    document.getElementById("preguntados").style.display = "block";
+}
 
-    <!-- 1ª pregunta -->
-    if (document.getElementById("p12").checked == true) {
-        p1 = 1;
+function myFunctionAvanzarTercera() {
+    if ((null != document.getElementById("r_l_21") && document.getElementById("r_l_21").checked) ||
+        (null != document.getElementById("r_e_21") && document.getElementById("r_e_21").checked)) {
+        resultados = resultados + 1;
+        console.log("resultado_2  " + resultados);
     }
-    if (p1 == 1) {
-        document.querySelectorAll(".pregunta1")[1].style.color = "#00FF00";
-    } else {
-        p1 = 0;
-    }
-    <!-- 2ª pregunta -->
-    if (document.getElementById("p21").checked == true) {
-        p2 = 1;
-    }
-    if (p2 == 1) {
-        document.querySelectorAll(".pregunta2")[0].style.color = "#00FF00";
-    } else {
-        p2 = 0;
-    }
-    <!-- 3ª pregunta -->
-    if (document.getElementById("p31").checked == true) {
-        p3 = 1;
-    }
-    if (p3 == 1) {
-        document.querySelectorAll(".pregunta3")[0].style.color = "#00FF00";
-    } else {
-        p3 = 0;
-    }
-    <!-- 4ª pregunta -->
-    if (document.getElementById("p43").checked == true) {
-        p4 = 1;
-    }
-    if (p4 == 1) {
-        document.querySelectorAll(".pregunta4")[2].style.color = "#00FF00";
-    } else {
-        p4 = 0;
-    }
-    <!-- 5ª pregunta -->
-    if (document.getElementById("p53").checked == true) {
-        p5 = 1;
-    }
-    if (p5 == 1) {
-        document.querySelectorAll(".pregunta5")[2].style.color = "#00FF00";
-    } else {
-        p5 = 0;
-    }
+    document.getElementById("preguntados").style.display = "none";
+    document.getElementById("preguntatres").style.display = "block";
+}
 
-    nota = p1 + p2 + p3 + p4 + p5;
-    document.getElementById("resultado").innerHTML = "Aciertos: " + nota;
-    //  rs = confirm("C o n t i n u a r . . .") CREAR UN ALERT;
-    rs = true;
-    if (rs == false) {
-        document.forms[0].reset();
-        location.reload();
+function myFunctionAvanzarCuarta() {
+    if ((null != document.getElementById("r_l_31") && document.getElementById("r_l_31").checked) ||
+        (null != document.getElementById("r_e_33") && document.getElementById("r_e_33").checked)) {
+        resultados = resultados + 1;
+        console.log("resultado_3  " + resultados);
     }
+    document.getElementById("preguntatres").style.display = "none";
+    document.getElementById("preguntacuatro").style.display = "block";
+}
+
+function myFunctionAvanzarQuinta() {
+    if ((null != document.getElementById("r_l_43") && document.getElementById("r_l_43").checked) ||
+        (null != document.getElementById("r_e_42") && document.getElementById("r_e_42").checked)) {
+        resultados = resultados + 1;
+        console.log("resultado_4  " + resultados);
+    }
+    document.getElementById("preguntacuatro").style.display = "none";
+    document.getElementById("preguntacinco").style.display = "block";
+    document.getElementById("boton_resultado").style.display = "block";
+}
+
+function myFunctionAvanzarSexta() {
+    if ((null != document.getElementById("r_l_53") && document.getElementById("r_l_53").checked) ||
+        (null != document.getElementById("r_e_51") && document.getElementById("r_e_51").checked)) {
+        resultados = resultados + 1;
+
+    }
+    document.getElementById("preguntacinco").style.display = "none";
+    document.getElementById("boton_resultado").style.display = "none";
+    console.log("resultado_5  " + resultados);
+    document.getElementById("resultado").style.display = "block";
+    document.getElementById("boton_retornar").style.display = "block";
+    document.getElementById("resultado").innerHTML = "resultados: " + resultados;
+}
+
+
+function myFunctionAtras() {
+
+    document.getElementById("preguntauno").style.display = "block";
+    document.getElementById("preguntados").style.display = "none";
+}
+
+function myFunctionAtras2() {
+    document.getElementById("preguntauno").style.display = "block";
+    document.getElementById("preguntados").style.display = "none";
+}
+
+function myFunctionAtras3() {
+    document.getElementById("preguntados").style.display = "block";
+    document.getElementById("preguntatres").style.display = "none";
+}
+
+function myFunctionAtras4() {
+    document.getElementById("preguntatres").style.display = "block";
+    document.getElementById("preguntacuatro").style.display = "none";
+
+}
+
+function myFunctionAtras5() {
+    document.getElementById("preguntacuatro").style.display = "block";
+    document.getElementById("preguntacinco").style.display = "none";
+
+}
+
+function retornar_categoria() {
+    var url_string = window.location.href; //
+    var url = new URL(url_string);
+    var usuario_url = url.searchParams.get("nombre");
+    location.href = "/pantalla_categoria.html?nombre=" + usuario_url
+}
+
+
+function pantalla_logica() {
+    var url_string = window.location.href; //
+    var url = new URL(url_string);
+    var usuario_url = url.searchParams.get("nombre");
+    location.href = "/pantalla_logica.html?nombre=" + usuario_url
+}
+
+function pantalla_educacion() {
+    var url_string = window.location.href; //
+    var url = new URL(url_string);
+    var usuario_url = url.searchParams.get("nombre");
+    location.href = "/pantalla_educacion.html?nombre=" + usuario_url
 }
